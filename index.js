157b1fc0f1cd51715ffe8a459ef221bd167ccb0b
Musa Issack <musa.m.issack@gmail.com>
2023-06-24 08:48:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-function addingEventListener() {
+const input = document.getElementById('input');
+
+function addingEventListener(){
+    function clickAlert(){
+        alert('I was touched')
+    }
+    input.addEventListener('click',clickAlert )
 }
+addingEventListener()
